647686c2e219c9cc6e92c06f11dd0bb44d095520
zhanghao639 <zhanghao639@pingan.com.cn>
2017-09-05 11:09:36 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # feather2-postpackager-offline
+
+feather2的资源离线方案，可以用于app下的离线html等hybrid应用。
+
+### 使用
+
+```js
+//conf.js
+feather.config.get('postpackager').push('offline');
+```
+
+本地预览，注：本地预览时，url仍为localhost，可方便使用本地的一些数据mock
+
+```sh
+feather2 release
+```
+
+正式编译时，则需要将static和view的代码放在同一个目录
+
+```js
+//conf/deploy/xxx.js 配置文件
+module.exports = [
+    {
+        from: '/view',
+        to: '../dist/',  //view和static需要同一目录
+        subOnly: true
+    },
+
+    {
+        from: '/static',
+        to: '../dist/',  //view和static需要同一目录
+        subOnly: true
+    }
+];
+```
+
+```sh
+feather2 release -d xxx
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var path = require('path');
+var merge = feather.util.merge;
+var pkgs;
+var M_PREFIX = feather.config.get('statics') + '/m_/';
+
+function is_M(url){
+    return url.indexOf(M_PREFIX) === 0;
+}
+
+function replaceMapFile(fileUrl, url){
+    var file;
+
+    for(var i in pkgs){
+        file = pkgs[i];
+
+        if(file.getUrl() === url){
+            break;
+        }
+    }
+
+    var obj = (new Function('var require = {config: function(obj){return obj}}; return ' + file.getContent()))();
+    var map = obj.map || {};
+    var newMap = {};
+
+    for(var i in map){
+        newMap[relative(fileUrl, i)] = map[i];
+    }
+
+    obj.map = newMap;
+    file.setContent('require.config(' + JSON.stringify(obj) + ');');
+}
+
+function relative(u1, u2){
+    if(u2[0] == '/'){
+        return path.relative(path.dirname(u1), u2).replace(/\\/g, '/');
+    }
+
+    return u2;
+}
+
+function replaceCss(fileUrl, content){
+    return content.replace(/\b((?:url|src)\(['"]?)([^'"\)]+)/g, function(all, prefix, url){
+        return prefix + relative(fileUrl, url);
+    });
+}
+
+function replaceHtml(fileUrl, content){
+    return content.replace(/(<style[^>]*>)([\s\S]+?)<\/style>/g, function(all, start, url){
+        return start + replaceCss(fileUrl, url) + '</style>';
+    }).replace(/(<[^>]+?(?:(?:data-)?href|src)=['"])([^'"]+)/g, function(all, start, url){
+        if(is_M(url)){
+            replaceMapFile(fileUrl, url);
+        }
+
+        return start + relative(fileUrl, url);
+    });
+}
+
+module.exports = function(ret){
+    pkgs = ret.pkg;
+
+    feather.util.map(merge(merge({}, ret.src), ret.pkg), function(subpath, file){
+        var content = file.getContent();
+
+        if(file.isCssLike){
+            content = replaceCss(file.getUrl(), content);
+            file.setContent(content);
+        }else if(file.isHtmlLike){
+            content = replaceHtml(file.subpath, content);
+            file.setContent(content);
+        }
+    })
+};
